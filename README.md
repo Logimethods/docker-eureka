a93c205029a80a1b121a9ddaa572c5c4bcec4a89
Laurent Magnin <laurent.magnin@logimethods.com>
2017-06-09 10:39:58 -0400

    [ci skip] services_node(node)

--- README.md
+++ README.md
@@ -4,12 +4,13 @@
 docker build -t eureka-local .
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -p 5000:5000 -e FLASK_DEBUG=1 logimethods/eureka
 
-docker service create --name registry --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock -p 5000:5000 -e FLASK_DEBUG=0 logimethods/eureka
+docker service create --name eureka --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock -p 5000:5000 -e FLASK_DEBUG=0 logimethods/eureka
 ```
 
 ```
 curl http://localhost:5000/container/id/<name>
 curl http://localhost:5000/service/id/<string:node>/<string:name>
+curl http://localhost:5000/services/node/<string:node>
 ```
 
 <name> can be based on a Python Regex: https://docs.python.org/3.6/library/re.html
--- docker_id.py
+++ docker_id.py
@@ -4,6 +4,7 @@
 
 import docker
 import re
+import json
 
 client = docker.from_env()
 
@@ -30,6 +31,13 @@ def get_tasks(node, name):
             selected.extend([addServiceName(d, service.name) for d in service.tasks({'node':node, 'desired-state':'running'})])
     return selected
 
+def get_tasks(node):
+    services = client.services.list()
+    tasks = []
+    for service in services:
+        tasks.append([addServiceName(d, service.name) for d in service.tasks({'node':node, 'desired-state':'running'})])
+    return tasks
+
 def get_container(name):
     pattern = re.compile(name)
     containers = client.containers.list()
@@ -37,6 +45,18 @@ def get_container(name):
         if pattern.match(container.name):
             return container
     return None
+
+def extract_container(service, task):
+    slot = task.get('Slot', task['NodeID'])
+    id = task['ID']
+    return service + '.' + str(slot) + '.' + str(id)
+
+def get_containers(node):
+    services = client.services.list()
+    containers = []
+    for service in services:
+        containers.append({service.name:[extract_container(service.name, d) for d in service.tasks({'node':node, 'desired-state':'running'})]})
+    return containers
 
 # RESTful server
 # http://containertutorials.com/docker-compose/flask-simple-app.html
@@ -90,6 +110,10 @@ def service_name(node, name):
     else:
         app.logger.debug('No service with a name as \'%s\' on %s node', name, node)
         return "None"
+
+@app.route('/services/node/<string:node>')
+def services_node(node):
+    return json.dumps(get_containers(node))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
